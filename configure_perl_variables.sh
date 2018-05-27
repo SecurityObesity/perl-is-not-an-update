546be4c0dcb10bcd8407a97ef5cc5551299a113e
dexoidan <dexoidan@gmail.com>
2018-05-27 20:15:14 +0200

    Add files via upload

---
+++ configure_perl_variables.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->commit; $c->edit(build_requires_install_policy => "yes"); $c->commit; $c->edit(show_unparsable_versions => "yes"); $c->commit;'
